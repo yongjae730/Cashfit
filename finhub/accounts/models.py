ea793793bbe727cd11aa063ed168ad452fc7ffbd
이용재 <lyj3023@naver.com>
2024-11-18 20:13:41 +0900

    user model field 추가

--- finhub/accounts/models.py
+++ finhub/accounts/models.py
@@ -4,4 +4,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    # 닉네임
+    nickname = models.CharField(max_length=10)
+    # 나이
+    age = models.PositiveIntegerField(null=True, blank=True, verbose_name="나이")
+    # 자본금 (만원 단위)
+    capital = models.PositiveIntegerField(null=True, blank=True, verbose_name="자본금")
+    
+
+    # user 출력 시 유저 이름으로 나오게
+    def __str__(self):
+        return self.username
--- finhub/accounts/views.py
+++ finhub/accounts/views.py
@@ -1,5 +1,15 @@
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 from django.shortcuts import render
 
 
 # Create your views here.
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "회원가입이 성공적으로 완료되었습니다!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
