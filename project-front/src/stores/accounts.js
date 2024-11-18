978fd07bdee5ca2eee57659231fe8933b6355fff
김병년 <qud5252@naver.com>
2024-11-18 14:38:07 +0900

    로그인, 로그아웃, 회원가입 기능 구현

---
+++ project-front/src/stores/accounts.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import router from "@/router";
+
+export const useAccount = defineStore(
+  "accounts",
+  () => {
+    const API_URL = "http://127.0.0.1:8000";
+    const token = ref(null);
+
+    const isLogin = computed(() => token.value !== null);
+
+    const login = async (payload) => {
+      try {
+        const { username, password } = payload;
+        const response = await axios.post(`${API_URL}/accounts/login/`, { username, password });
+        token.value = response.data.key;
+        router.push({ name: "Main" });
+      } catch (err) {
+        console.error("Login failed:", err);
+      }
+    };
+
+    const signUp = async (payload) => {
+      try {
+        const { username, password1, password2 } = payload;
+        await axios.post(`${API_URL}/accounts/signup/`, { username, password1, password2 });
+        await login({ username, password: password1 });
+      } catch (err) {
+        console.error("Signup failed:", err.response?.data || err);
+      }
+    };
+
+    return { API_URL, token, isLogin, login, signUp };
+  },
+  { persist: true }
+);
