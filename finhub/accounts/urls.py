75846459bbe42c17faf3a1a4908bbe1ebfa0ca5a
김병년 <qud5252@naver.com>
2024-11-18 11:53:55 +0900

    충돌 해결

--- finhub/accounts/urls.py
+++ finhub/accounts/urls.py
@@ -2,10 +2,5 @@ from django.urls import path,include
 from . import views
 app_name = 'accounts'
 urlpatterns = [
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout"),
-    path('signup/', views.signup, name="signup"),
-    path('delete/', views.delete, name="delete"),
-    path('update/', views.update, name='update'),
-    path('password_change/', views.password_change, name='password_change'),
+
 ]
