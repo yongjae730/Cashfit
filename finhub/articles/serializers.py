3076d61aa1fca6399367d455979a3544dcab368d
이용재 <lyj3023@naver.com>
2024-11-19 11:38:33 +0900

    user serializer update

--- finhub/articles/serializers.py
+++ finhub/articles/serializers.py
@@ -8,11 +8,10 @@ class ArticleListSerializer(serializers.ModelSerializer):
 
         
 class ArticleSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('user',)
+        read_only_fields = ('users',)
 
 class ArticleCommentSerializer(serializers.ModelSerializer):
     class Meta:
