7644f7d58d96baf0b93061fa779f9f6bee75784b
KimByeongNyeon <qud5252@naver.com>
2024-11-22 02:39:50 +0900

    데이터 로딩 시 로딩 스피너 추가

--- project-front/src/views/FinView.vue
+++ project-front/src/views/FinView.vue
@@ -6,18 +6,22 @@
           <v-icon icon="mdi-bank" size="large" class="mr-2" color="primary" />
           <span class="text-h5 font-weight-bold">금융 상품</span>
           <v-spacer />
-          <v-text-field v-model="search" prepend-inner-icon="mdi-magnify" label="검색" single-line hide-details
-            density="compact" variant="outlined" class="max-width-200" />
+          <v-text-field v-model="search" prepend-inner-icon="mdi-magnify" label="검색" single-line hide-details density="compact" variant="outlined" class="max-width-200" />
         </v-card-title>
 
         <v-divider />
 
-        <div v-if="finList.length > 0" class="custom-table-container">
-          <v-data-table :headers="headers" :items="finList || []" :search="search" hover fixed-header
-            height="calc(100vh - 250px)" class="financial-table elevation-1" :items-per-page="-1">
+        <!-- 로딩 상태 -->
+        <div v-if="isLoading" class="text-center">
+          <v-progress-circular indeterminate color="primary" />
+          <p>데이터를 불러오는 중입니다...</p>
+        </div>
+
+        <!-- 데이터 테이블 -->
+        <div v-else-if="finList.length > 0" class="custom-table-container">
+          <v-data-table :headers="headers" :items="finList || []" :search="search" hover fixed-header height="calc(100vh - 250px)" class="financial-table elevation-1" :items-per-page="-1">
             <template #item.fin_prdt_nm="{ item }">
-              <router-link :to="`/product/${item.fin_prdt_cd}`" @click="setProduct(item)"
-                class="text-decoration-none font-weight-medium text-primary">
+              <router-link :to="`/product/${item.fin_prdt_cd}`" @click="setProduct(item)" class="text-decoration-none font-weight-medium text-primary">
                 {{ item.fin_prdt_nm }}
               </router-link>
             </template>
@@ -25,10 +29,7 @@
             <!-- 옵션 열 -->
             <template #item.options="{ item }">
               <ul>
-                <li v-for="(option, index) in item.options" :key="index">
-                  {{ option.intr_rate_type_nm }}: {{ option.intr_rate }}% ~ {{ option.intr_rate2 }}% ({{ option.save_trm
-                  }}개월)
-                </li>
+                <li v-for="(option, index) in item.options" :key="index">{{ option.intr_rate_type_nm }}: {{ option.intr_rate }}% ~ {{ option.intr_rate2 }}% ({{ option.save_trm }}개월)</li>
               </ul>
             </template>
           </v-data-table>
@@ -49,6 +50,7 @@ const setProduct = (item) => {
   store.setSelectedProduct(item);
 };
 const search = ref("");
+const isLoading = ref(true); // 로딩 상태 추가
 
 const headers = [
   {
@@ -79,13 +81,13 @@ const headers = [
     title: "옵션",
     align: "center",
     value: "options",
-    width: "120px", // 옵션 열을 넓게 설정
+    width: "120px",
   },
   {
-    title: "종류", // 새로운 열 추가
+    title: "종류",
     align: "center",
     value: "product_type_display",
-    width: "80px"
+    width: "80px",
   },
   {
     title: "기타",
@@ -95,21 +97,18 @@ const headers = [
   },
 ];
 
-const finList = ref([]);  // 금융 상품 목록
+const finList = ref([]); // 금융 상품 목록
 
 onMounted(async () => {
   try {
     const response = await axios.get("http://127.0.0.1:8000/api/financials/financial-products-with-options/");
-    console.log("API 응답 데이터:", response.data); // 응답 확인
+    console.log("API 응답 데이터:", response.data);
     if (Array.isArray(response.data)) {
-      // 데이터 가공
-      finList.value = response.data.map(item => {
-        const productTypeDisplay = item.product_type === 0
-          ? "예금"
-          : item.options[0]?.rsrv_type_nm || "정보 없음"; // rsrv_type_nm 또는 기본값
+      finList.value = response.data.map((item) => {
+        const productTypeDisplay = item.product_type === 0 ? "예금" : item.options[0]?.rsrv_type_nm || "정보 없음";
         return {
           ...item,
-          product_type_display: productTypeDisplay // 새 필드 추가
+          product_type_display: productTypeDisplay,
         };
       });
     } else {
@@ -117,21 +116,18 @@ onMounted(async () => {
     }
   } catch (error) {
     console.error("Failed to fetch data:", error);
+  } finally {
+    isLoading.value = false; // 로딩 완료
   }
 });
-
-
 </script>
 
 <style>
-/* 전역 스타일로 설정 */
 .v-data-table-footer {
   display: none !important;
 }
 
 .custom-table-container {
-
-  /* 스크롤바 스타일링 */
   & ::-webkit-scrollbar {
     width: 8px !important;
     height: 8px !important;
